846f66c0431016ab37bbcff910ab343b2bd42aae
Kearabetswe <keahjabane@gmail.com>
2025-02-02 21:45:26 +0200

    Used Array Types on Typescript File

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 var person = {
     name: 'Precious',
-    age: 26
+    age: 26,
+    hobbies: ['sporting', 'cooking']
 };
+var favoriteActivities;
+favoriteActivities = ['sporting'];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
--- app.ts
+++ app.ts
@@ -1,8 +1,17 @@
 const person:{
     name:string;
     age:number;
+  hobbies: string[]
 } = {
     name: 'Precious',
-    age: 26
-}
+    age: 26,
+   hobbies: ['sporting','cooking']
+};
+let favoriteActivities :string[];
+favoriteActivities = ['sporting'];
+
 console.log(person.name);
+
+for(const hobby of person.hobbies){
+    console.log(hobby)
+}
