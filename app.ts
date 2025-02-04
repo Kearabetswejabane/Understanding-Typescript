099d338c533dd8b75fc54c7a19dcc145e5c066d5
Kearabetswe <keahjabane@gmail.com>
2025-02-04 18:49:23 +0200

    Creating Type Aliases/Custom Types

--- app.ts
+++ app.ts
@@ -1,4 +1,8 @@
-function add(input1:string|number,input2:string|number,resultConversion: 'as-number'|'as-text'){
+type combinable = number|string
+type ConversionDescriptor = 'as-number'|'as-text'
+
+
+function add(input1:combinable,input2:combinable,resultConversion: ConversionDescriptor){
     let result; 
     if(typeof input1 === 'number' && typeof input2 === 'number'|| resultConversion === 'as-number'){
       result = +input1 + +input2  
